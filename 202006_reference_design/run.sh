6b85f8455f72dde840dac4f3e734723dae7c25fd
Andrea Zonca <code@andreazonca.com>
2020-06-28 20:34:13 -0700

    run all channels

--- 202006_reference_design/run.sh
+++ 202006_reference_design/run.sh
@@ -1,1 +1,1 @@
-python $(which s4_design_sim_tool) $1 --channels LAT --site all --output_folder output/${1/.toml//}
+python $(which s4_design_sim_tool) $1 --channels all --site all --output_folder output/${1/.toml//}
