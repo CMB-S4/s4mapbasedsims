354cb18450df4bb7f34da26e61b3c843ef7ed389
Andrea Zonca <code@andreazonca.com>
2020-07-07 21:12:24 -0700

    support splits

--- 202006_reference_design/run_plots.sh
+++ 202006_reference_design/run_plots.sh
@@ -1,3 +1,3 @@
 while read ch; do
-   papermill -p ch $ch validation_atmo_noise.ipynb validation_notebooks/$ch.ipynb 
-done <LAT_chs.txt
+   papermill -p ch $ch -p nsplits 7 validation_atmo_noise.ipynb validation_notebooks/$ch.ipynb 
+done <all_chs.txt
