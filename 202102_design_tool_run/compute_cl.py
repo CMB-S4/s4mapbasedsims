74f170e2a83216415bb39bece6c690f03e3046b6
Andrea Zonca <code@andreazonca.com>
2021-03-10 10:30:33 -0800

    improved wcov weighting

--- 202102_design_tool_run/compute_cl.py
+++ 202102_design_tool_run/compute_cl.py
@@ -5,18 +5,21 @@ import pickle
 import h5py
 from pathlib import Path
 
-from compute_cl_utils import build_pol_weights
+from compute_cl_utils import build_inverse_cov_weights
 
 ellmax = int(1e4)
+from astropy.table import QTable
 
-s4 = h5py.File("cmbs4_tophat.h5", mode="r")
 cl = {}
 telescope = sys.argv[1]
 splits = int(sys.argv[2])
-sites = ["chile"]
+sites = ["pole"]
 
-local_folder = Path("output/") # local
-output_base_folder = Path("/global/project/projectdirs/cmbs4/dm/dstool/output/") # project
+s4 = QTable.read("instrument_model/cmbs4_instrument_model.tbl", format="ascii.ipac")
+s4 = s4[s4["telescope"] == telescope]
+
+local_folder = Path("output/")  # local
+output_base_folder = Path("output/")  # project
 output_base_folder = local_folder
 
 
@@ -25,17 +28,18 @@ for folder in output_base_folder.glob("*noise*"):
     output_filename = local_folder / folder.name / f"C_ell_{telescope}_{splits}.pkl"
     for site in sites:
 
-        for ch in [c for c in s4.keys() if s4[c].attrs["telescope"] == telescope]:
+        for row in s4:
+            ch = row["band"]
             tag = f"{telescope}-{ch}_{site}"
             ch_folder = folder / tag
             wcov_filenames = list(ch_folder.glob(f"*wcov*1_of_{splits}*"))
-            assert(len(wcov_filenames) == 1)
+            assert len(wcov_filenames) == 1
             wcov_filename = wcov_filenames[0]
-            pol_weights = build_pol_weights(wcov_filename):
+            temp_weights, pol_weights, noise_cl = build_inverse_cov_weights(wcov_filename)
 
             print(tag)
             cl[tag] = {}
-            for s in range(1, splits+1):
+            for s in range(1, splits + 1):
                 try:
                     filenames = list(ch_folder.glob(f"*KCMB*{s}_of_{splits}*"))
                     assert len(filenames) == 1
@@ -51,11 +55,15 @@ for folder in output_base_folder.glob("*noise*"):
                     m = hp.ma(hp.read_map(filename))
                     nside = hp.npix2nside(len(m))
 
-                m *= pol_weights
-                norm = np.mean(pol_weights**2)
-                del pol_weights
+                m[0] *= temp_weights
+                m[1:] *= pol_weights
                 assert np.all(np.isfinite(m))
-                cl[tag][s] = hp.anafast(m, lmax=min(3 * nside - 1, ellmax), use_pixel_weights=True) / norm
+                cl[tag][s] = hp.anafast(
+                    m, lmax=min(3 * nside - 1, ellmax), use_pixel_weights=True
+                )
+
+                cl[tag][s][0] /= np.mean(temp_weights ** 2)
+                cl[tag][s][1:] /= np.mean(pol_weights ** 2)
 
     with open(output_filename, "wb") as f:
         pickle.dump(cl, f, protocol=-1)
--- 202102_design_tool_run/compute_cl_utils.py
+++ 202102_design_tool_run/compute_cl_utils.py
@@ -1,12 +1,15 @@
 import healpy as hp
 import numpy as np
 
-def build_pol_weights(wcov_filename):
+
+def build_inverse_cov_weights(wcov_filename):
     """Build inverse variance weights for polarization from covariance matrices"""
-    wcov = hp.ma(hp.read_map(wcov_filename, (3, 4, 5)))
-    pol_weights = 2/(wcov[0] + wcov[2] + 2*wcov[1])
+    wcov = hp.ma(hp.read_map(wcov_filename, (0, 3, 4, 5)))
+    pol_weights = 2 / (wcov[1] + wcov[3] + 2 * wcov[2])
     pol_weights = pol_weights.filled(0)
+    temp_weights = (1 / wcov[0]).filled(0)
     assert np.all(np.isfinite(pol_weights))
     pixarea = hp.nside2pixarea(hp.npix2nside(wcov.shape[1]))
-    noise_cl = np.average(wcov, weights=pol_weights**2, axis=0) * pixarea
-    return pol_weights, noise_cl
+    noise_cl = np.average(wcov, weights=pol_weights ** 2, axis=1) * pixarea
+    noise_cl[0] = np.average(wcov[0], weights=temp_weights ** 2) * pixarea
+    return temp_weights, pol_weights, noise_cl
