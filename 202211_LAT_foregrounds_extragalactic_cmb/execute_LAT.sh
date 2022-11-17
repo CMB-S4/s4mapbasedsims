9fc21ef036b08414697f1fbb08c25b7166eb4dc6
Andrea Zonca <code@andreazonca.com>
2022-11-17 14:20:13 -0800

    improve threading

--- 202211_LAT_foregrounds_extragalactic_cmb/execute_LAT.sh
+++ 202211_LAT_foregrounds_extragalactic_cmb/execute_LAT.sh
@@ -1,9 +1,12 @@
 NSIDE=4096
-export OMP_NUM_THREADS=68
 #for cfg in cib.toml # ksz.toml tsz.toml radio.toml cmb.toml cmb_unlensed.toml
 # for cfg in cmb.toml cmb_unlensed.toml
 export PYTHONUNBUFFERED=1
-for cfg in ame.toml freefree.toml
+export OMP_PROC_BIND=true
+export OMP_PLACES=threads
+export OMP_NUM_THREADS=68
+export HDF5_USE_FILE_LOCKING=FALSE
+for cfg in dust.toml
 do
     mapsims_run --verbose --channels='telescope:LAT' --nside=$NSIDE common.toml $cfg
 done
