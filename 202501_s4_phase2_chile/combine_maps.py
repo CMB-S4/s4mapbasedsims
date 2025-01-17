b2a7081300f5f74edaa624e22bba4f1db993bea6
Andrea Zonca <code@andreazonca.com>
2025-01-17 15:38:47 -0800

    copy of 202410_s4_phase1_chile

---
+++ 202501_s4_phase2_chile/combine_maps.py
@@ -0,0 +1,96 @@
+import os
+import healpy as hp
+import numpy as np
+from astropy.table import QTable
+from pathlib import Path
+
+extragalactic = ["cib_cib1", "ksz_ksz1", "tsz_tsz1"]
+all_combined = {
+    "combined_cmb_unlensed_dipole": ["cmb_c3", "dipole"],
+    "combined_cmb_lensing_signal": ["cmb_c4", "-cmb_c3"],
+    "combined_foregrounds_mediumcomplexity_norg": [
+        "dust_d10",
+        "synchrotron_s5",
+        "freefree_f1",
+        "ame_a1",
+        "co_co3",
+    ]
+    + extragalactic,
+    "combined_foregrounds_lowcomplexity_norg": [
+        "dust_d9",
+        "synchrotron_s4",
+        "freefree_f1",
+        "ame_a1",
+        "co_co1",
+    ]
+    + extragalactic,
+    "combined_foregrounds_highcomplexity_norg": [
+        "dust_d12",
+        "synchrotron_s7",
+        "freefree_f1",
+        "ame_a2",
+        "co_co3",
+    ]
+    + extragalactic,
+}
+
+s4 = QTable.read(
+    "instrument_model/cmbs4_instrument_model.tbl",
+    format="ascii.ipac",
+)
+
+num = 0
+output_folder = Path("output")
+
+for output_content, components in all_combined.items():
+    for row in s4:
+        band = row["band"]
+        print(band)
+        nside = 512 if row["telescope"] == "SAT" else 4096
+        output_filename = (
+            output_folder
+            / f"{output_content}/cmbs4_{output_content}_uKCMB_{band}_nside{nside}.fits"
+        )
+        if os.path.exists(output_filename):
+            print("skip", output_filename)
+        else:
+            combined_map = np.zeros((3, hp.nside2npix(nside)), dtype=np.float64)
+            for content in components:
+                print(content)
+                sign = 1
+                if content.startswith("-"):
+                    sign = -1
+                    content = content[1:]
+                if content == "dipole":
+                    filename = (
+                        output_folder
+                        / f"dipole/cmbs4_cmb_solar_dipole_uKCMB_nside{nside}.fits"
+                    )
+                else:
+                    filename = (
+                        output_folder
+                        / f"{content}/cmbs4_{content}_uKCMB_{band}_nside{nside}.fits"
+                    )
+                try:
+                    m = hp.read_map(
+                        filename, dtype=np.float64, field=(0, 1, 2), nest=True
+                    )
+                    combined_map += sign * m
+                except IndexError:
+                    print("T only map")
+                    combined_map[0] += sign * hp.read_map(
+                        filename, dtype=np.float64, field=0, nest=True
+                    )
+            os.makedirs(os.path.dirname(output_filename), exist_ok=True)
+            hp.write_map(
+                output_filename,
+                combined_map,
+                nest=True,
+                coord="C",
+                column_units="uK_CMB",
+                overwrite=True,
+                dtype=np.float32,
+            )
+            print(20 * "*")
+            print(output_filename)
+            print(20 * "*")
---
+++ 202501_s4_phase2_chile/verification/README.md
@@ -0,0 +1,17 @@
+# Simulation verification
+
+## CO Lines
+
+[Verify CO Lines notebook](verify_CO.ipynb)
+
+## Power spectra
+
+All components except CO and Radio Galaxies,
+the B Spectra of the CMB components are affected by high noise due to `ud_grade` performed in the on-the-fly run with PySM inside the notebook.
+If the run is properly executed at Nside 4096, no artificial noise is visible anymore.
+* [SAT T](https://nbviewer.org/gist/zonca/3f26ab742241764db18f24629c81a83e)
+* [SAT E](https://nbviewer.org/gist/zonca/08a0d25c677fdac169888f35ed171dda)
+* [SAT B](https://nbviewer.org/gist/zonca/62328ba870a1618968361c8248715196)
+* [CHLAT T](https://nbviewer.org/gist/zonca/5b50a78dfd9a490f57bf090ddc2c5bba)
+* [CHLAT E](https://nbviewer.org/gist/zonca/d15b3f7cda586d1409e0e8ad05215c01)
+* [CHLAT B](https://nbviewer.org/gist/zonca/f0e638767b948e5673a898d598e1ff1a)
